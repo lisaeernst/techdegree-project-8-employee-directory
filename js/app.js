13ac0a848df72f4465e5573e922dd172b743f00a
lisaeernst <78939274+lisaeernst@users.noreply.github.com>
2021-06-03 17:17:46 -0700

    uploading files

---
+++ js/app.js
@@ -0,0 +1,13 @@
+/* Awesome Startup Employee Directory */
+
+// randomuser.me data
+
+/* $.ajax({
+    url: 'https://randomuser.me/api/',
+    dataType: 'json',
+    success: function(data) {
+      console.log(data);
+    }
+  }); */
+
+  
